ba21ee5a7fd64416a6ef319e37e31c6885e985b2
Samuel Johnston <samueljohnston96@gmail.com>
2015-11-15 19:11:33 -0500

    Commented code

--- scheduler/Rater.go
+++ scheduler/Rater.go
@@ -1,12 +1,14 @@
 package scheduler
 
 import "time"
-
+// Struct to check if a user is checking a website too frequently
 type Rater struct {
+	// Map of website names to last check times
 	LastChecked map[string]time.Time
+	// Largest amount of time allowed between each check
 	AcceptableLimit time.Duration
 }
-
+// Checks if you can access the website based on the Acceptable Limit
 func (rates Rater) CanRun(website string ) bool {
 	return time.Since(rates.LastChecked[website]) >= rates.AcceptableLimit
 }
--- scheduler/Rater_test.go
+++ scheduler/Rater_test.go
@@ -5,14 +5,19 @@ import (
 	"testing"
 	"github.com/opinionated/scheduler/scheduler"
 )
-
+// Tests to make sure the Rater works properly
 func TestRater(T *testing.T){
+	// creates a map to test corner cases on
 	mymap := make(map[string]time.Time)
+	// Sets upper limit for time ahead allowed
 	var timeleft time.Duration = 22 * time.Hour
+	// Tests cases for Current time, day after, and day before
 	mymap["NewyorkTimes.com"] = time.Now()
 	mymap["Swagmoney.com"] = (time.Now()).AddDate(0,0,1)
 	mymap["Cnn.com"] = (time.Now()).AddDate(0,0,-1)
+	// Makes the struct
 	var m scheduler.Rater = scheduler.Rater{mymap, timeleft}
+	// Tests to make sure CanRun Works for these values
 	if m.CanRun("NewyorkTimes.com"){
 		T.Errorf("Day Before Failed\n")
 	}
